2b587a825ef9791472ee7444c788ad179c71041f
Erux <danutzelu@gmail.com>
2019-03-27 12:11:33 +0200

    add comments

--- study_pytest/test_addition.py
+++ study_pytest/test_addition.py
@@ -2,6 +2,7 @@ import pytest
 
 
 # param tests using pytest parametrize
-@pytest.mark.parametrize("input1, input2, output", [(5,5,10),(5,3,12)])
+@pytest.mark.parametrize("input1, input2, output", [(5, 5, 10), (5, 3, 12)])
+# test method accepts three arguments: input1, input2 and output
 def test_add(input1, input2, output):
     assert input1 + input2 == output, "fail; reason: {} + {} does not equal {}".format(input1, input2, output)
