ac75b6708bdfa30eb36c4f2bcfc2dc66493077e8
Erux <danutzelu@gmail.com>
2019-03-27 10:53:49 +0200

    update error message for failed assert

--- study_pytest/test_sample1.py
+++ study_pytest/test_sample1.py
@@ -5,7 +5,8 @@ def test_file1_method1():
     x = 5
     y = 6
     assert x+1 == y, "test failed"
-    assert x == y, "test failed"
+    # an error message will be displayed when assertions fail
+    assert x == y, 'test failed because x = {} and y = {}'.format(x, y)
 
 
 def test_file1_method2():
@@ -13,4 +14,3 @@ def test_file1_method2():
     y = 6
     assert x + 1 == y, "test failed"
 
-    
