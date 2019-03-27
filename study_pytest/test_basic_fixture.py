c069b7841121b6dc7e0ee3c029f0fd1dc81a21df
Erux <danutzelu@gmail.com>
2019-03-27 11:55:49 +0200

    remove fixture function from test file

--- study_pytest/test_basic_fixture.py
+++ study_pytest/test_basic_fixture.py
@@ -1,14 +1,6 @@
 import pytest
 
 
-# pytest fixture function - this will be called before any test that uses it as an argument
-@pytest.fixture
-def supply_AA_BB_CC():
-    aa = 25
-    bb = 35
-    cc = 45
-    return[aa, bb, cc]
-
 def test_comparewithAA(supply_AA_BB_CC):
     zz = 35
     assert supply_AA_BB_CC[0] == zz, "aa and zz comparison failed"
