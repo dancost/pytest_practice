35fd946c620f50ffc4ef0e5b171f1f7f7df0e5c7
Erux <danutzelu@gmail.com>
2019-03-27 11:03:03 +0200

    add import statements and markers to test functions

--- study_pytest/test_sample1.py
+++ study_pytest/test_sample1.py
@@ -1,14 +1,16 @@
 import pytest
 
 
+@pytest.mark.set1
 def test_file1_method1():
     x = 5
     y = 6
-    assert x+1 == y, "test failed"
+    assert x + 1 == y, "test failed"
     # an error message will be displayed when assertions fail
     assert x == y, 'test failed because x = {} and y = {}'.format(x, y)
 
 
+@pytest.mark.set2
 def test_file1_method2():
     x = 5
     y = 6
--- study_pytest/test_sample2.py
+++ study_pytest/test_sample2.py
@@ -1,12 +1,16 @@
+import pytest
 
+
+# pytest markers allow us to run different sets of tests, based on marker names
+@pytest.mark.set1
 def test_file2_method1():
     x = 5
     y = 6
-    assert x+1 == y, "test failed"
-    # an error message will be displayed when assertions fail
+    assert x + 1 == y, "test failed"
     assert x == y, 'test failed because x = {} and y = {}'.format(x, y)
 
 
+@pytest.mark.set1
 def test_file2_method2():
     x = 5
     y = 6
